f0a8126c5470980ad26dc1d9be872726c544c9a3
meimei (milagros) <meixinliu1997@gmail.com>
2024-06-22 22:12:57 -0500

    update homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,32 @@
 
 import "./Home.css";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
+
+    const [products] = useState([
+        {
+            id: 1,
+            name: "Ensalada de rocas lunares",
+            price: 24.90,
+            quantity: 2,
+            image: "link_to_image",
+        },
+        {
+            id: 2,
+            name: "Tallarin de Kalamari",
+            price: 43.90,
+            quantity: 2,
+            image: "https://media.licdn.com/dms/image/D4D12AQH6pZSSpjla3A/article-inline_image-shrink_1000_1488/0/1679606233057?e=1723680000&v=beta&t=cbvAxi9VatABV-bB_xLanBvydoNdPSm7TE8K_jpIR5k",
+        },
+        {
+            id: 3,
+            name: "Bebida GoSpace",
+            price: 2.90,
+            quantity: 1,
+            image: "link_to_image",
+        },
+    ]);
 
     return(
         <div className="homepage">
@@ -29,21 +53,14 @@ export default function Home() {
         <section className="best-selling-products">
           <h2>PRODUCTOS MÁS VENDIDOS</h2>
           <div className="products">
-            <div className="product">
-              <div className="product-image">
-                <img alt="Tallarin de Kalamari" src= "https://media.licdn.com/dms/image/D4D12AQH6pZSSpjla3A/article-inline_image-shrink_1000_1488/0/1679606233057?e=1723680000&v=beta&t=cbvAxi9VatABV-bB_xLanBvydoNdPSm7TE8K_jpIR5k"/>
+            {products.map(item => (
+                <div className="product-image" key={item.id}>
+                    <img src={item.image} alt={item.name} />
+                    <div className="product">
+                        <h4>{item.name}</h4>
+                    </div>
                 </div>
-
-              <div className="product-description">Tallarin de Kalamari</div>
-            </div>
-            <div className="product">
-              <div className="product-image" />
-              <div className="product-description">Descripción del producto</div>
-            </div>
-            <div className="product">
-              <div className="product-image" />
-              <div className="product-description">Descripción del producto</div>
-            </div>
+                ))}
           </div>
         </section>
       </main>
